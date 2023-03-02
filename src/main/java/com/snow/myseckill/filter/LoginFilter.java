fa74f16fcbbe42840987fa3f7ce5647be640113b
snowMan <snowmanazz@gmail.com>
2023-03-02 19:41:55 +0800

    添加过滤器，过滤登录请求

--- src/main/java/com/snow/myseckill/filter/LoginFilter.java
+++ src/main/java/com/snow/myseckill/filter/LoginFilter.java
@@ -1,4 +1,4 @@
-package com.snow.myseckill.interceptor;
+package com.snow.myseckill.filter;
 
 import com.snow.myseckill.pojo.User;
 import com.snow.myseckill.redis.UserKeyPrefix;
@@ -6,27 +6,35 @@ import com.snow.myseckill.service.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URLDecoder;
 
 @Slf4j
 @Component
-public class LoginInterceptor implements HandlerInterceptor {
+public class LoginFilter implements Filter {
 
     @Autowired
     private RedisService redisService;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("初始化拦截器-------");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
         String uri = request.getRequestURI();
         log.info("请求的地址--[{}]", uri);
         Cookie[] cookies = request.getCookies();
-        if (uri.matches("/user/.*")) {
-            return true;
+        if (uri.matches("/user/.*") && !uri.equals("/user/checkLogin")) {
+            return;
         }
         for (Cookie cookie : cookies) {
             if (cookie != null && UserKeyPrefix.TOKEN.getprefix().equals(URLDecoder.decode(cookie.getName(), "utf-8"))) {
@@ -34,7 +42,7 @@ public class LoginInterceptor implements HandlerInterceptor {
                 User user = redisService.get(UserKeyPrefix.TOKEN, token, User.class);
                 if (user != null) {
                     log.info("用户已经登陆--{}", user);
-                    return true;
+                    return;
                 }
             }
         }
@@ -42,7 +50,10 @@ public class LoginInterceptor implements HandlerInterceptor {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json;charset=utf-8");
         log.error("请先登录..");
-        return false;
     }
 
+    @Override
+    public void destroy() {
+        log.info("销毁拦截器---------");
+    }
 }
