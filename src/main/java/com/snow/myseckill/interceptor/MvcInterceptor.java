1133fd78c1f5f72ed1d3d811ff34182112dd4ce7
ming zhou <snowmnazz@gmail.com>
2023-03-06 17:03:03 +0800

    拦截器返回错误信息

--- src/main/java/com/snow/myseckill/interceptor/MvcInterceptor.java
+++ src/main/java/com/snow/myseckill/interceptor/MvcInterceptor.java
@@ -1,8 +1,14 @@
 package com.snow.myseckill.interceptor;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.rabbitmq.tools.json.JSONUtil;
 import com.snow.myseckill.pojo.User;
+import com.snow.myseckill.result.CodeMsg;
+import com.snow.myseckill.result.Result;
 import com.snow.myseckill.service.RedisService;
 import com.snow.myseckill.service.UserService;
+import com.snow.myseckill.util.ConvertUtil;
 import com.snow.myseckill.util.CookieUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +18,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -27,7 +36,12 @@ public class MvcInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = CookieUtil.getCookieToken(request, redisService);
         User user = userService.getUserByToken(token);
-        return user != null;
+        if (user == null) {
+            returnJson(response);
+            return false;
+        }
+        return true;
+
     }
 
     @Override
@@ -36,5 +50,23 @@ public class MvcInterceptor implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+
+
+    private void returnJson(HttpServletResponse response) {
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            Result<CodeMsg> result = Result.error(CodeMsg.SESSION_ERROR);
+            writer.print(JSONObject.toJSONString(result));
+        } catch (IOException e) {
+            log.error("拦截器输出流异常[{}]", e.getMessage());
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
     }
 }
